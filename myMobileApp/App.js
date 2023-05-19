223a988b16289304d3590a43187db1f309a1a037
Volodymyr Bashtanyuk <volodymyrbashtanyuk@MacBook-Air-Volodymyr.local>
2023-05-19 16:25:32 +0300

    add same button android and ios

--- myMobileApp/App.js
+++ myMobileApp/App.js
@@ -1,21 +1,31 @@
+import { useState } from 'react';
+// import React {useState} from 'react';
 
-import { StyleSheet, Text, View, ImageBackground, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity, Platform, KeyboardAvoidingView} from 'react-native';
 
 export default function App() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
   return (
     <View style={styles.container}>
+
       <ImageBackground style={styles.image} source={require('./assets/images/image-app.jpeg')}>
-        <View style={styles.form}>
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+
+      <View style={{...styles.form, marginBottom: isShowKeyboard ? 50: 10}}>
           <View>
             <Text style={styles.imputTitle}>Email adress</Text>
-            <TextInput style={styles.input} textAlign={'center'} />
+            <TextInput style={styles.input} textAlign={'center'} onFocus={()=>setIsShowKeyboard(true)}/>
           </View>
           <View style={{marginTop: 20}}>
             <Text style={styles.imputTitle}>Password</Text>
-            <TextInput style={styles.input} textAlign={'center'} secureTextEntry={true} />
+            <TextInput style={styles.input} textAlign={'center'} secureTextEntry={true} onFocus={()=>setIsShowKeyboard(true)}/>
           </View>
-          <Button title='SING IN'/>
-        </View>
+          <TouchableOpacity activeOpacity={0.8} style={styles.btn} >
+              <Text style={styles.btnTitle}>SIGN IN</Text>
+          </TouchableOpacity>
+          </View>
+      </KeyboardAvoidingView>
+          
        
       </ImageBackground>
 
@@ -37,6 +47,7 @@ const styles = StyleSheet.create({
   },
   form: {
     marginHorizontal: 20,
+    // marginBottom: 50,
 
   },
   imputTitle: {
@@ -49,5 +60,28 @@ const styles = StyleSheet.create({
     height: 40,
     borderRadius: 10,
     color: '#f0f8ff',
+  },
+  btn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 40,
+    marginTop: 30,
+    borderRadius: 10,
+    marginHorizontal: 20,
+    ...Platform.select({
+      ios: {
+        backgroundColor: '#1e90ff',
+        
+      },
+      android: {
+        backgroundColor: '#0000ff',
+
+      }
+    })
+
+  },
+  btnTitle: {
+    color: '#f0f8ff',
+    fontSize: 18,
   }
 });
